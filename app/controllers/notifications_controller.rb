09c1c5b08d0d0bd4b114f4ac5b13a20677c54278
MaxLievense <max1lievense@gmail.com>
2022-05-23 19:38:09 +0200

    Revert "Auto adds creator of notification closes #3"
    This reverts commit 85ec278e6007d431d2a15cd1dd19c7f9acab0416.

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -22,7 +22,6 @@ class NotificationsController < ApplicationController
   # POST /notifications or /notifications.json
   def create
     @notification = Notification.new(notification_params)
-    @notification.user_id = current_user.id
 
     respond_to do |format|
       if @notification.save
